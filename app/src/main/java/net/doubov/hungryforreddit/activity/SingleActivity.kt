6b838d155d5a014a1e0f7f83ef24a9b7c6a0968f
Alex Doubov <alex.doubov@autodesk.com>
2020-02-08 10:17:26 -0800

    Removing stuff to make room for workflow

--- app/src/main/java/net/doubov/hungryforreddit/activity/SingleActivity.kt
+++ app/src/main/java/net/doubov/hungryforreddit/activity/SingleActivity.kt
@@ -1,32 +1,71 @@
 package net.doubov.hungryforreddit.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentFactory
-import net.doubov.hungryforreddit.App
+import com.squareup.workflow.RenderContext
+import com.squareup.workflow.Snapshot
+import com.squareup.workflow.StatefulWorkflow
+import com.squareup.workflow.diagnostic.SimpleLoggingDiagnosticListener
+import com.squareup.workflow.ui.*
+import com.squareup.workflow.ui.LayoutRunner.Companion.bind
 import net.doubov.hungryforreddit.R
-import net.doubov.hungryforreddit.di.SingleActivityBuilder
-import javax.inject.Inject
+
+
+object RootWorkflow : StatefulWorkflow<Unit, RootWorkflow.State, Nothing, RootWorkflow.Rendering>() {
+
+    sealed class State {
+        object Init : State()
+    }
+
+    data class Rendering(val blah: String)
+
+    override fun initialState(props: Unit, snapshot: Snapshot?): State = State.Init
+    override fun snapshotState(state: State): Snapshot = Snapshot.EMPTY
+
+    override fun render(props: Unit, state: State, context: RenderContext<State, Nothing>): Rendering {
+        return Rendering("HIHI")
+    }
+
+}
+
+class RootLayoutRunner(view: View) : LayoutRunner<RootWorkflow.Rendering> {
+
+    companion object : ViewBinding<RootWorkflow.Rendering> by bind(R.layout.activity_main, ::RootLayoutRunner)
+
+    private val text = view.findViewById<TextView>(R.id.welcome)
+
+    override fun showRendering(rendering: RootWorkflow.Rendering, containerHints: ContainerHints) {
+        text.text = rendering.blah
+    }
+}
 
 class SingleActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var router: SingleActivityRouter
-
-    @Inject
-    lateinit var fragmentFactory: FragmentFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val rootBuilder = SingleActivityBuilder(this, App.getApp(this).getAppComponent())
-        rootBuilder.build()
-
-        supportFragmentManager.fragmentFactory = fragmentFactory
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            router.goToMainParentFragment()
-        }
+        setContentWorkflow(
+            registry = viewRegistry,
+            configure = {
+                WorkflowRunner.Config(
+                    RootWorkflow,
+                    diagnosticListener = SimpleLoggingDiagnosticListener()
+                )
+            },
+            onResult = {
+                finish()
+            }
+        )
+    }
+
+
+    private companion object {
+        private val viewRegistry = ViewRegistry(
+            RootLayoutRunner
+        )
     }
 }
--- app/src/main/java/net/doubov/hungryforreddit/activity/SingleActivityRouter.kt
+++
@@ -1,5 +0,0 @@
-package net.doubov.hungryforreddit.activity
-
-interface SingleActivityRouter {
-    fun goToMainParentFragment()
-}
